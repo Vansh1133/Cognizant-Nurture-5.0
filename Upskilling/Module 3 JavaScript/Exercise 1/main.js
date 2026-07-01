0ecb3e1ef1199d5ba9fb36b816a0d55bf2714493
Vansh1133 <vanshsharma77890@gmail.com>
2026-07-01 13:15:10 +0530

    A

--- Upskilling/Module 3 JavaScript/Exercise 1/main.js
+++
@@ -1,7 +0,0 @@
-// Display a message in the browser console
-console.log("Welcome to the Community Portal");
-
-// Show an alert when the page is fully loaded
-window.onload = function () {
-    alert("Page loaded successfully!");
-};
